9bb9fade90d5dc50457f057a988ae81ace3d6d76
yrys_nm <yrys.n3m@gmail.com>
2022-07-01 21:16:37 +0600

    new files

---
+++ src/apis/youtubeStatistics.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const KEY = 'AIzaSyBFuoKvDQD46B--9zCW2dsvcfx2tmb4Xgg'; // mention your youtube API key here
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'statistics',
+        key: KEY
+    }
+})
